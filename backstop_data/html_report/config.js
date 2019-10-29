eeb8790dec9683fbe7df135e0602099e1bb45938
rutenis <rutis98@gmail.com>
2019-10-29 14:55:47 +0000

    use readySelector instead of delay

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Material_Card_0_document_0_phone.png",
-        "test": "../bitmaps_test/20191029-115454/backstop_default_Material_Card_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191029-143737/backstop_default_Material_Card_0_document_0_phone.png",
         "selector": "document",
         "fileName": "backstop_default_Material_Card_0_document_0_phone.png",
         "label": "Material Card",
@@ -20,15 +20,17 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "2.41",
+          "analysisTime": 23
+        },
+        "diffImage": "../bitmaps_test/20191029-143737/failed_diff_backstop_default_Material_Card_0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Material_Card_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20191029-115454/backstop_default_Material_Card_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191029-143737/backstop_default_Material_Card_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "backstop_default_Material_Card_0_document_1_tablet.png",
         "label": "Material Card",
@@ -44,17 +46,17 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.46",
-          "analysisTime": 25
+          "misMatchPercentage": "0.47",
+          "analysisTime": 30
         },
-        "diffImage": "../bitmaps_test/20191029-115454/failed_diff_backstop_default_Material_Card_0_document_1_tablet.png"
+        "diffImage": "../bitmaps_test/20191029-143737/failed_diff_backstop_default_Material_Card_0_document_1_tablet.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Material_Card_0_document_2_laptop.png",
-        "test": "../bitmaps_test/20191029-115454/backstop_default_Material_Card_0_document_2_laptop.png",
+        "test": "../bitmaps_test/20191029-143737/backstop_default_Material_Card_0_document_2_laptop.png",
         "selector": "document",
         "fileName": "backstop_default_Material_Card_0_document_2_laptop.png",
         "label": "Material Card",
@@ -71,9 +73,9 @@ report({
             "height": 0
           },
           "misMatchPercentage": "0.36",
-          "analysisTime": 36
+          "analysisTime": 42
         },
-        "diffImage": "../bitmaps_test/20191029-115454/failed_diff_backstop_default_Material_Card_0_document_2_laptop.png"
+        "diffImage": "../bitmaps_test/20191029-143737/failed_diff_backstop_default_Material_Card_0_document_2_laptop.png"
       },
       "status": "fail"
     }
